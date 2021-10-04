5c9130072a4bf0f8ff8f50ff1f693b76a9923c6a
RishabhSahil <91429171+RishabhSahil@users.noreply.github.com>
2021-10-05 03:10:16 +0530

    update
    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('frmsearch').onsubmit = function() {
-    window.location = 'search.html#gsc.tab=0&gsc.q='+  document.getElementById('textSearch').value;
-    return false;		
+    window.location = 'search.html#gsc.tab=0&gsc.q=' + document.getElementById('textSearch').value;
+    return false;
 }
