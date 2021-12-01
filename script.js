19520b3a7b83ea03e6a339e3ce9c0d1a7a64da53
RishabhSahil <91429171+RishabhSahil@users.noreply.github.com>
2021-12-01 22:34:00 +0530

    v21h2

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 document.getElementById('frmsearch').onsubmit = function() {
-    window.location = 'search.html#gsc.tab=0&gsc.q=' + document.getElementById('textSearch').value;
-    return false;
+    query = document.getElementById('textSearch').value;
+    if(query==false){
+        // alert("Please Enter Your Query")
+        console.log("please enter your query");
+        return false
+    }
+    else{
+        window.location = 'search.html#gsc.tab=0&gsc.q=' + query
+        return false;
+
+    }
+
 }
